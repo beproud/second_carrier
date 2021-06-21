2a3d83d3b1bebf9b061c686293f552d92152cd22
wan <nishikawa0228@sj9.so-net.ne.jp>
2021-06-21 12:45:53 +0900

    [#1] コーチ一覧取得APIの実装

--- second_carrier/lesson/views.py
+++ second_carrier/lesson/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import CoachSerializer
+from .models import Coach
+
+
+class CoachView(generics.ListAPIView):
+    """コーチ一覧API
+
+    https://tracery.jp/s/3b41ac0d4d334a2d9a2a68eba29d1f86
+    """
+    serializer_class = CoachSerializer
+
+    def get_queryset(self):
+        return Coach.objects.all().order_by("-review_point")
